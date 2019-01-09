67cf7423733a933bc6594707fb813c1727aa1276
momo <1445107381@qq.com>
2019-01-09 20:30:29 +0800

    add twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,29 @@
+// 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+// 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+
+// 示例:
+
+// 给定 nums = [2, 7, 11, 15], target = 9
+
+// 因为 nums[0] + nums[1] = 2 + 7 = 9
+
+// 所以返回 [0, 1]
+
+var twoSum = function(nums, target) {
+    let result=[];
+    for(let i=0;i<nums.length;i++){
+        for(let j=0;j<nums.length;j++){
+            if(nums[j]=== target-nums[i]){ //不能重复利用这个数组中同样的元素
+                if(i !==j){
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+    }
+  
+};
+twoSum([3,6,4,5],7);
+console.log( twoSum([3,6,4,5],7) );
